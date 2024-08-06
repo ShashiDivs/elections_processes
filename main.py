30e59836e5600484f28232933d1780e67fa760b7
shashi <gaajarlasheshugoud@gmail.com>
2024-08-06 19:48:13 +0530

    updated

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
-st.markdown("""
-    <style>
-    .css-1r6slb0 {display:none;}
-    </style>
-    """, unsafe_allow_html=True)
-# Add custom CSS to hide the GitHub icon
 hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
+<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK, 
+#MainMenu, footer, header {
+  display: none !important;  // Ensures these elements are completely removed from view
 }
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 
 
 import matplotlib.pyplot as plt
